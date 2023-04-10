074051082e13b4c75b718c32fb645b720c95c918
toledopaulo <paulo.aff1803@gmail.com>
2023-04-10 20:35:46 -0300

    files added

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyCandies - Home</title>
+    <link rel="stylesheet" href="static/css/home.css">
+    <link rel="stylesheet" href="static/css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div id="topbar">
+        <label class="topbar-title">MyCandies</label>
+        <ul class="topbar-items">
+            <li><a href="views/shop.php">Shop</a></li>
+            <li><a href="views/contact.php">Contact</a></li>
+            <li><a href="views/news.php">News</a></li>
+            <li><a href="views/about.php">About us</a></li>
+        </ul>
+    </div>
+    <div id="main-content">
+        <div id="introduction">
+            <img src="static/imgs/mycandies.webp" id="mycandies-logo">
+            <p class="subtitle">More than a candy shop</p>
+        </div>
+        <a href="views/shop.php" id="shop-now-btn" class="btn btn-primary">Shop now </a>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
